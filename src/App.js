a2ba5556084401593e365719ae98734478b4c0f2
maximiloi <maximiloi@gmail.com>
2020-12-03 16:11:22 +0300

    4 урок

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route } from 'react-router-dom';
+
+import { Header } from './components';
+import { Home, Cart } from './pages';
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cart" component={Cart} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
